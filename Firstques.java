72d2a6b4576222fe0b20795c5e87d67fc09c35ac
ParviM <89266514+ParviM@users.noreply.github.com>
2021-08-20 21:43:03 +0530

    Add files via upload

---
+++ Firstques.java
@@ -0,0 +1,45 @@
+package org.ibm.training;
+
+public class Firstques {
+	class animal{
+		int age;
+		String gender;
+		public void isMammal() {
+			
+		}
+	
+	public void mate() {
+		
+	}
+}
+class Duck extends animal{
+	public String beakColor="yellow";
+	public void swim() {
+		
+	}
+	public void quack() {
+		
+	}
+
+}
+class Fish extends animal{
+	private int size;
+	private void swim() {
+		
+	}
+}
+class Zebra extends animal{
+	public boolean is_wild;
+	public void run() {
+		
+	}
+}
+public static void main(String[] args) {
+	
+	}
+}
+
+
+
+
+	
---
+++ Secondques.java
@@ -0,0 +1,5 @@
+package org.ibm.training;
+
+public class Secondques {
+
+}
